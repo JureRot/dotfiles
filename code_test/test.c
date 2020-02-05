88dad7097d7ca2185dea1434c718ec48c1413602
Jure Rot <jurerot@Jures-MacBook-Pro.local>
2020-02-05 20:14:54 +0100

    nvim setup start

---
+++ code_test/test.c
@@ -0,0 +1,12 @@
+#import <stdio.h>
+
+void bla(int n) {
+	for (int i=0; i<n; i++) {
+		printf("bla\n");
+	}
+}
+
+int main(int argc, char *argv[]) {
+	//this is comment
+	bla(4);
+}
---
+++ code_test/test.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class test {
+	static void bla(int n, float b, int a) {
+		System.out.println("bla");
+	}
+
+	public static void main(String[] args) {
+		int one = 1;
+		int two = 2;
+		float three = 3;
+
+		bla(one, three, two);
+
+	}
+
+
+}
